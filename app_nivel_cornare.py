2a712b2c675b53c4a917c045d5f8caf9dae9a12d
Alejandrovr13 <alejandro.vizcaino031@pascualbravo.edu.co>
2026-08-31 13:44:51 -0500

    Update app_nivel_cornare.py

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -28,6 +28,10 @@ CANDIDATOS_LAT = ["lat", "latitude", "latitud"]
 CANDIDATOS_LON = ["lng", "lon", "longitude", "longitud"]
 
 st.set_page_config(page_title="Estacion Abejorral, Quebrada La Aduanilla", page_icon="🌊", layout="wide")
+st.imagen(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmEJbWfs5eD2K-Qcpocl9u47bKqy_V5091dzGEIvzP1yGx6mIG_vjy5P8&s=10",
+    width =250
+)
 
 
 def obtener_serie_nivel(codigo_estacion, desde, hasta, calidad=1, timeout=30):
